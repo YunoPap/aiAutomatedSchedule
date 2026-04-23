baa0b4d609f89a20b0fc07b85b8a3fea8c14aaaf
Yuno <stepevade23@gmail.com>
2026-04-23 17:45:15 -0400

    fixing

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-i = 0
+import pandas as pd
+
+
+def getCompanyRoles():
+    return companyRoles
+
+
+def load_data():
+    pass
+
+def get_ai_scheduler():
+    pass
+
+companyRoles = [
+    "CEO",
+    "Chef",
+    "Manager",
+    "Cook",
+    "Waiter",
+    "Runner",
+    "Dishwasher",
+    "Bartender",
+    "Host",
+]
